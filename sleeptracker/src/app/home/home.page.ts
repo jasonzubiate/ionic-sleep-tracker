2100ad67ce0821573e7282391eba9c0f9a70a94e
jasonzubiate <jzubiate@uci.edu>
2022-11-21 14:14:01 -0800

    latest changes

--- sleeptracker/src/app/home/home.page.ts
+++ sleeptracker/src/app/home/home.page.ts
@@ -1,19 +1,24 @@
-import { Component } from '@angular/core';
-import { SleepService } from '../services/sleep.service';
-import { SleepData } from '../data/sleep-data';
-import { OvernightSleepData } from '../data/overnight-sleep-data';
-import { StanfordSleepinessData } from '../data/stanford-sleepiness-data';
+import { Component } from "@angular/core";
+import { SleepService } from "../services/sleep.service";
+import { SleepData } from "../data/sleep-data";
+import { OvernightSleepData } from "../data/overnight-sleep-data";
+import { StanfordSleepinessData } from "../data/stanford-sleepiness-data";
+import { TimerComponent } from "../components/timer/timer.component";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+	selector: "app-home",
+	templateUrl: "home.page.html",
+	styleUrls: ["home.page.scss"],
 })
 export class HomePage {
+	trackerStatus: string = "inactive";
+	counter: number;
+	timerRef;
+	loggedTime;
+	startTime;
+	elapsedTimeStr = "8 hours 30 minutes";
 
-	constructor(public sleepService:SleepService) {
-
-	}
+	constructor(public sleepService: SleepService) {}
 
 	ngOnInit() {
 		console.log(this.allSleepData);
@@ -24,4 +29,27 @@ export class HomePage {
 		return SleepService.AllSleepData;
 	}
 
+	startTracker() {
+		this.trackerStatus = "active";
+		if (this.trackerStatus == "active") {
+			this.startTime = Date.now() - (this.counter || 0);
+			this.timerRef = setInterval(() => {
+				this.counter = Date.now() - this.startTime;
+			});
+		} else {
+			clearInterval(this.timerRef);
+			this.clearTrackerTimer()
+		}
+	}
+
+	clearTrackerTimer() {
+		this.trackerStatus = "inactive";
+		this.loggedTime = new OvernightSleepData(this.startTime, this.loggedTime);
+	}
+
+	async trackerToggle() {
+		this.trackerStatus == "inactive"
+			? this.startTracker()
+			: this.clearTrackerTimer();
+	}
 }
